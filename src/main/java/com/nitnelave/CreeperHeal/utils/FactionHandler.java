163571d83999b2aca6001feced81038c561c0532
Jikoo <jikoo.games@gmail.com>
2021-06-17 12:40:50 -0400

    Support FactionsUUID via reflection

--- src/main/java/com/nitnelave/CreeperHeal/utils/FactionHandler.java
+++ src/main/java/com/nitnelave/CreeperHeal/utils/FactionHandler.java
@@ -1,12 +1,14 @@
 package com.nitnelave.CreeperHeal.utils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.jikoo.planarwrappers.function.ThrowingTriFunction;
+import org.bukkit.Chunk;
 import org.bukkit.block.Block;
 
-import com.massivecraft.factions.entity.BoardColl;
-import com.massivecraft.massivecore.ps.PS;
 import com.nitnelave.CreeperHeal.PluginHandler;
 import com.nitnelave.CreeperHeal.config.WCfgVal;
 import com.nitnelave.CreeperHeal.config.WorldConfig;
@@ -21,10 +23,21 @@ public abstract class FactionHandler
 {
 
     private static boolean isFactionsEnabled = false;
+    private static Method boardSingleton;
+    private static Method boardGetFaction;
+    private static Method factionIsWilderness;
+    private static ThrowingTriFunction<String, Integer, Integer, Object, ReflectiveOperationException> getLocation;
 
     static
     {
         setFactionsEnabled(PluginHandler.isFactionsEnabled());
+    }
+
+    private static boolean isWilderness(Block block) throws Exception {
+        Chunk chunk = block.getChunk();
+        Object faction = boardGetFaction.invoke(boardSingleton.invoke(null),
+            getLocation.apply(chunk.getWorld().getName(), chunk.getX(), chunk.getZ()));
+        return faction != null && !(boolean) factionIsWilderness.invoke(faction);
     }
 
     /**
@@ -36,6 +49,48 @@ public abstract class FactionHandler
     public static void setFactionsEnabled(boolean enabled)
     {
         isFactionsEnabled = enabled;
+        if (enabled) {
+            setup();
+        }
+    }
+
+    private static void setup() {
+        if (!isFactionsEnabled) {
+            return;
+        }
+
+        // Set up FactionsUUID.
+        try {
+            Class<?> boardClazz = Class.forName("com.massivecraft.factions.Board");
+            boardSingleton = boardClazz.getMethod("getInstance");
+            Class<?> factionLocationClazz = Class.forName("com.massivecraft.factions.FLocation");
+            boardGetFaction = boardClazz.getMethod("getFactionAt", factionLocationClazz);
+            Constructor<?> locationConstructor = factionLocationClazz.getConstructor(String.class, int.class, int.class);
+            getLocation = locationConstructor::newInstance;
+            Class<?> factionClazz = Class.forName("com.massivecraft.factions.Faction");
+            factionIsWilderness = factionClazz.getMethod("isWilderness");
+
+            // FactionsUUID set up successfully, we're done.
+            return;
+        } catch (ReflectiveOperationException e) {
+            // Eat first reflection error - may be MassiveCraft Factions.
+        }
+
+        try
+        {
+            // MassiveCraft's (discontinued) Factions
+            Class<?> massiveBoard = Class.forName("com.massivecraft.factions.entity.BoardColl");
+            boardSingleton = massiveBoard.getDeclaredMethod("get");
+            Class<?> massivePS = Class.forName("com.massivecraft.massivecore.ps.PS");
+            boardGetFaction = massiveBoard.getDeclaredMethod("getFactionAt", massivePS);
+            Method locationValueOf = massivePS.getDeclaredMethod("valueOf", String.class, int.class, int.class);
+            getLocation = (worldName, chunkX, chunkZ) -> locationValueOf.invoke(null, worldName, chunkX, chunkZ);
+            Class<?> massiveFaction = Class.forName("com.massivecraft.factions.entity.Faction");
+            factionIsWilderness = massiveFaction.getDeclaredMethod("isNone");
+        } catch (ReflectiveOperationException e) {
+            // Factions failed to set up, disable Factions support.
+            isFactionsEnabled = false;
+        }
     }
 
     /**
@@ -52,13 +107,24 @@ public abstract class FactionHandler
         if (!isFactionsEnabled)
             return false;
 
-        boolean wild = world.getBool(WCfgVal.FACTIONS_IGNORE_WILDERNESS), territory = world.getBool(WCfgVal.FACTIONS_IGNORE_TERRITORY);
+        boolean wild = world.getBool(WCfgVal.FACTIONS_IGNORE_WILDERNESS);
+        boolean territory = world.getBool(WCfgVal.FACTIONS_IGNORE_TERRITORY);
         if (wild == territory)
             return wild;
 
-        for (Block block : list)
-            if (wild != BoardColl.get().getFactionAt(PS.valueOf(block.getLocation())).isNone())
-                return false;
+        try
+        {
+            for (Block block : list)
+            {
+                if (wild != isWilderness(block))
+                {
+                    return false;
+                }
+            }
+        } catch (Exception e)
+        {
+            return false;
+        }
         return true;
     }
 
